1c2ab2729b84b496725a29e230f184c5126cdc53
Nightfurex <83917263+Nightfurex@users.noreply.github.com>
2022-02-27 12:12:28 +0530

    init

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Pass
 import pygame
 import random
 pygame.init()
@@ -7,8 +8,12 @@ class DrawingInfomartion():
     WHITE = 255,255,255
     GREEN = 0,255,0
     RED = 255,0,0
-    GREY = 128,128,128
     BACKGROUND_COLOR = WHITE
+    GRADIENTS = [
+        (128,128,128),
+        (160,160,160),
+        (192,192,192)
+    ]
     SIDE_PAD = 100
     TOP_PAD = 150
 
@@ -28,6 +33,21 @@ class DrawingInfomartion():
         self.start_x = self.SIDE_PAD // 2
 
 
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    draw_list(draw_info)
+    pygame.display.update()
+
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+    for i,val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+        color = draw_info.GRADIENTS[i % 3]
+        pygame.draw.rect(draw_info.window,color,(x,y, draw_info.block_width,draw_info.height))
+
+
 def generate_starting_list(n,max_value,min_value):
     lst = []
     for _ in range(n):
@@ -37,7 +57,7 @@ def generate_starting_list(n,max_value,min_value):
 
 def main():
     run = True
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
     n = 60
     min_value = 0
     max_value = 100
@@ -47,6 +67,7 @@ def main():
     while run:
         clock.tick(60)
         pygame.display.update()
+        draw(draw_info)
 
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
